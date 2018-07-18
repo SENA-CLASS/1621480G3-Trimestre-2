ed5d844d01fd43a9b2d1f6e883031b26bde4ad17
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-07-18 14:13:49 -0500

    chore: ejemplo tipos de datos

---
+++ mysql/DDL.sql
@@ -0,0 +1,12 @@
+-- DDL
+create database ejemplo01;
+drop database ejemplo01;
+ALTER SCHEMA `ejemplo2`  DEFAULT CHARACTER SET cp1250  DEFAULT COLLATE cp1250_general_ci ;
+
+-- tipos de datos
+create table ejemplo2.numericos(
+	campo1 int,
+    campo2 int unsigned
+
+);
+
---
+++ mysql/basicos.sql
@@ -0,0 +1,10 @@
+-- comandos basicos
+show databases;
+use ejemplo;
+show tables;
+
+show engines;
+
+create database ejemplo2;
+use ejemplo2;
+create schema ejemploesquema; -- en mysql hace lo mismo que create database
