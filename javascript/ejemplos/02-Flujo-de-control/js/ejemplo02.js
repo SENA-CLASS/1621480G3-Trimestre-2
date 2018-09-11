8f07747172ebdd627fed58203331c29d699c229d
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-09-11 13:17:00 -0500

    chore: switch ejemplo 2

---
+++ javascript/ejemplos/02-Flujo-de-control/js/ejemplo02.js
@@ -0,0 +1,18 @@
+(function () {
+    let opcion = prompt("digite su letra");
+
+
+    switch (opcion) {
+        case "A":
+        case "a":
+            alert("usted selecciono A o a");
+            break;
+        case "B":
+        case "b":
+            alert("usted selecciono B o b");
+            break;
+        default:
+            alert("esta opción no la conosco");
+
+    }
+})();
